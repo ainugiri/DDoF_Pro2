be6066be027abf80e08c91c5a73415c137f7d25b
ainugiri <40005294+ainugiri@users.noreply.github.com>
2023-09-20 11:41:59 +0530

    Maven Added

---
+++ atos.print/src/test/java/atos/atos/print/AppTest.java
@@ -0,0 +1,20 @@
+package atos.atos.print;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
